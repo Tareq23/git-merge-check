5fe2649739e23c7486e53e3aa34377c68d65a557
Tareq23 <mdtarequlislam634@gmail.com>
2022-03-15 21:36:22 +0600

    user register

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|max:100',
+            'email' => 'required|unique:users',
+            'contact_number' => 'required|max:14|min:11',
+            'password' => 'required|min:8',
+        ],[
+            'name.required' => 'Required Name!',
+            'email.required' => 'Required Email',
+            'email.unique' => 'Your email already exists',
+            'contact_number' => 'Required phone number'
+        ]);
+
+        // if(count($validator->fails()))
+        if($validator->fails())
+        {
+            return response()->json([
+                'error' => $validator->errors(),
+            ]);
+            // return response()->withErrors($validator)->withInput();
+        }
+        return response()->json([
+            'succress' => true
+        ]);
+    }
+}
--- database/migrations/2022_03_15_133025_create_users_table.php
+++ database/migrations/2022_03_15_133025_create_users_table.php
@@ -15,11 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name',100);
+            $table->string('email',100)->unique();
+            $table->string('contact_number',15)->unique();
+            $table->string('password',200);
             $table->timestamps();
         });
     }
